7aabf068d1ef5f67c12c74c4967663c4166998a9
anerocha <anerocha1999@gmail.com>
2024-11-03 17:38:02 +0000

    Adiciona dados de gasolina e gráfico

--- gasolina.py
+++ gasolina.py
@@ -1,1 +1,19 @@
-# (Copie e cole o código acima aqui para que ele seja salvo em gasolina.py)
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Ler os dados do CSV
+data = pd.read_csv('gasolina.csv')
+
+# Criar o gráfico
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=data, x='dia', y='venda', marker='o')
+plt.title('Preço Médio de Venda da Gasolina em São Paulo (Julho 2021)')
+plt.xlabel('Dia')
+plt.ylabel('Preço (R$)')
+plt.legend(['Preço da Gasolina'])
+plt.grid(True)
+
+# Salvar o gráfico
+plt.savefig('gasolina.png')
